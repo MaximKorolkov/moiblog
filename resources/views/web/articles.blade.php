7cbc0561368b6877650cafd4344307b58d58f997
MaximKorolkov <kormaxim93@mail.ru>
2018-03-22 21:01:53 +0400

    Add html comment and rubric

--- resources/views/web/articles.blade.php
+++ resources/views/web/articles.blade.php
@@ -7,18 +7,27 @@
 @include('web.header')
 
 @section('layout')
+
+    <article class="article">
+        <header class="article__header">
+            @if($article->image != '')
+                <div class="article__header-bg" style="background-image: url({{$article->image}})"  >
+                    <div class="line-read">
+                    <h1 class="article__header__h1">{!! $article->header_h1 !!}</h1>
+                    </div>
+                    </div>
+            @endif
+        </header>
+    <div class="container">
     <div class="row">
        <div class="col-sm-12">
-           <article class="article">
-                <header class="article__header">
-                    <h1 class="article__header__h1">{!! $article->header_h1 !!}</h1>
                     <time class="article__header__time"
                           datetime="{{ $article->created_at  }}"
                           title="Время создания статьи : {{  $date }}">
                         {{  $date   }}
                     </time>
                     <span class="article__header__author"><span>{!! $article->author !!}</span></span>
-                </header>
+
 
                 <div class="article__text">
                     {!! $article->description  !!}
@@ -27,14 +36,14 @@
 
             <footer class="article__footer">
                 @if(isset($article->per_post) and isset($article->per_post_url))
-                 <a class="article_footer__prev" href="{!! $article->per_post_url !!}">
+                 <a class="article_footer__prev" href="{{ '/article' . '/' . $article->per_post_url }}">
                      <i class="glyphicon glyphicon-backward"></i>
                      {!! $article->per_post !!}
                  </a>
                  @endif
-                <span class="article__footer__space">  || </span>
+
                 @if(isset($article->next_post) and isset($article->next_post_url) )
-                 <a class="article__footer__next" href="{!!  $article->next_post_url !!}">
+                 <a class="article__footer__next" href="{{ '/article' . '/' .  $article->next_post_url }}">
                      {!! $article->next_post !!}
                      <i class="glyphicon glyphicon-forward"></i>
                  </a>
@@ -42,12 +51,15 @@
                  <a class="article__footer__comment" href="#">Коментарии</a>
 
             </footer>
-               </article>
            @include('web.pieces.comment')
+
+
+          @foreach($rubrics as $rubric)
+               <a href="{{'/rubric' .'/'.$rubric->url}}">{{  $rubric->name  }}</a>
+           @endforeach
        </div>
-
-
-
-
     </div>
+    </div>
+
+    </article>
     @endsection
